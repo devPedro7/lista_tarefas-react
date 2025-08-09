f402ae87507a26243dd160e7f16123bc80569104
Pedro Lucas <devpedro37@gmail.com>
2025-08-09 20:33:57 -0300

    Adicionando o corpo do projeto

--- LISTA DE TAREFAS/src/App.jsx
+++ LISTA DE TAREFAS/src/App.jsx
@@ -1,35 +1,48 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      categoria: "Trabalho",
+      isComplete: false
+    },
+    {
+      id: 2,
+      text: "Ir na academia",
+      categoria: "Pessoal",
+      isComplete: false
+    },
+        {
+      id: 3,
+      text: "Comprar lembrancinha pra Mari",
+      categoria: "Pessoal",
+      isComplete: false
+    }
+  ])
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+  return <div className='app'>
+
+    <h1>Lista de tarefas</h1>
+
+    <div className='todo-list'>
+      {todos.map((todo)=>(
+        <div className="todo">
+          <div className="content">
+            <p>{todo.text}</p>
+            <p className='category'>({todo.categoria})</p>
+          </div>
+          <div>
+            <button>Completar</button>
+            <button>X</button>
+          </div>
+        </div>
+        
+      ))}
+    </div>
+  </div>
 }
 
 export default App
